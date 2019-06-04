d0eddc1c9ddff51f440a43190a88d518c1e24d94
Conceptx <theophilusokoye6@gmail.com>
2019-06-04 09:45:46 +0200

    async fix

--- routes/getStatistics.js
+++ routes/getStatistics.js
@@ -2,17 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const types = ["theft", "dui", "assault", "battery", "rape", "hit and run", "robbery", "arson", "burglary"];
-    const results = [];
-    const query = Parse.Object.extend('Cases');
+	const types = [
+		'theft',
+		'dui',
+		'assault',
+		'battery',
+		'rape',
+		'hit and run',
+		'robbery',
+		'arson',
+		'burglary'
+	];
+	const results = [];
+	const query = Parse.Object.extend('Cases');
 
-    types.map((value, i) => {
-        query.equalTo('type', value);
-        const count = await query.count();
-        results.push(count);
-    });
+	types.map(async (value, i) => {
+		query.equalTo('type', value);
+		const count = await query.count();
+		results.push(count);
+	});
 
-    res.json({success: true, results})
+	res.json({ success: true, results });
 });
 
 router.get('/:type', async (req, res) => {
